2dba08f9f7b13376227176f564188eb1c7569a89
Chris Charles <chris@charles-linux.ouac.on.ca>
2009-05-28 09:44:10 -0400

    Fixed local color-theme modifications to work with nonstandard faces.

--- emacs.d/init.d/50color-theme.el
+++ emacs.d/init.d/50color-theme.el
@@ -12,4 +12,15 @@
 (if (require 'color-theme-wombat nil t)
     (progn
       (color-theme-wombat)
-      (set-face-background 'highlight-current-line-face "grey10")))
+
+      ; Make highlight-current-line-face look OK with wombat
+      (eval-after-load "highlight-current-line"
+        '(set-face-background 'highlight-current-line-face "grey10"))
+
+      ; Make cperl hashes and array slook OK with wombat
+      (eval-after-load "cperl-mode"
+        '(progn
+           (set-face-background 'cperl-array-face nil)
+           (set-face-foreground 'cperl-array-face "#cae682")
+           (set-face-background 'cperl-hash-face nil)
+           (set-face-foreground 'cperl-hash-face "#cae682")))))
