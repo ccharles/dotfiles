0985a818308b83067c683f86cdd1bd908b67cdf3
Chris Charles <chris@charles-linux.ouac.on.ca>
2009-06-03 16:07:41 -0400

    Added custom whitespace-mode colours for wombat.

--- emacs.d/init.d/50color-theme.el
+++ emacs.d/init.d/50color-theme.el
@@ -23,4 +23,16 @@
            (set-face-background 'cperl-array-face nil)
            (set-face-foreground 'cperl-array-face "#cae682")
            (set-face-background 'cperl-hash-face nil)
-           (set-face-foreground 'cperl-hash-face "#cae682")))))
+           (set-face-foreground 'cperl-hash-face "#cae682")))
+
+      ; Make the whitespace characters look OK with wombat
+      (eval-after-load "whitespace"
+        '(progn
+           (set-face-background 'whitespace-space nil)
+           (set-face-foreground 'whitespace-space "grey30")
+           (set-face-background 'whitespace-newline nil)
+           (set-face-foreground 'whitespace-newline "grey30")
+           (set-face-background 'whitespace-tab nil)
+           (set-face-foreground 'whitespace-tab "grey30")
+           (set-face-background 'whitespace-indentation nil)
+           (set-face-foreground 'whitespace-indentation "grey30")))))
