b5ebc640acb7d0c165af61f9e295aeeef04201de
Chris Charles <chris@charles-linux.ouac.on.ca>
2009-11-20 15:49:04 -0500

    Added ERC logging.

--- emacs.d/init.d/50erc.el
+++ emacs.d/init.d/50erc.el
@@ -12,4 +12,13 @@
 (eval-after-load "erc"
   '(progn
      (set 'erc-nick "cccharles")
-     (set 'erc-user-full-name "Chris Charles")))
+     (set 'erc-user-full-name "Chris Charles")
+
+     ;; From http://nflath.com/2009/10/bitlbee-and-emacs/
+     (setq erc-log-channels-directory "~/.emacs.d/erc-logs"
+           erc-save-buffer-on-part nil
+           erc-save-queries-on-quit nil
+           erc-log-write-after-send t
+           erc-log-write-after-insert t)
+
+     (erc-log-mode 1)))
