7ebbcf23ad516a252e57dd524fd7d68f2024c704
Chris Charles <chris@ccharles.ca>
2011-06-27 15:35:51 -0400

    In Awesome, prevent the number of master windows from going below 1.

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -206,7 +206,13 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
     awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
+    awful.key({ modkey, "Shift"   }, "l",     function ()
+                                                 -- Always have at least one
+                                                 -- master window
+                                                 if awful.tag.getnmaster() > 1 then
+                                                    awful.tag.incnmaster(-1)
+                                                 end
+                                              end),
     awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
     awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
