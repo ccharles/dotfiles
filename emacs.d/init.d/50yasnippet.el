7aa94bb5ee1d3cfc25451f0b386c4d8954de6fa6
Chris Charles <chris@ccharles.ca>
2011-07-27 09:18:21 -0400

    Added Arch default yasnippet directory.

--- emacs.d/init.d/50yasnippet.el
+++ emacs.d/init.d/50yasnippet.el
@@ -11,7 +11,9 @@
 
 (if (require 'yasnippet nil t)
     (progn (yas/initialize)
-		   (setq yas/trigger-key [C-tab])
-		   (defvar snippets-dir "~/.snippets/")
-		   (if (file-readable-p snippets-dir)
-			   (yas/load-directory snippits-dir))))
+           ;; This doesn't seem to work for some reason...
+           ;; (setq yas/trigger-key (kbd "C-<tab>"))
+           (yas/load-directory "/usr/share/emacs/site-lisp/yas/snippets")
+           (defvar local-snippets-dir "~/.snippets")
+           (if (file-readable-p local-snippets-dir)
+               (yas/load-directory local-snippets-dir))))
