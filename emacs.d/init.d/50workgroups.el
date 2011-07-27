675a730720a5e6c7d97d349798a0ff1941f0743c
Chris Charles <chris@ccharles.ca>
2011-07-27 11:28:34 -0400

    Added Emacs workgroups config file.

---
+++ emacs.d/init.d/50workgroups.el
@@ -0,0 +1,27 @@
+(if (require 'workgroups nil t)
+    (progn
+      (workgroups-mode 1)
+      (wg-load "~/.emacs.d/workgroups")
+
+      (eval-after-load "workgroups"
+        '(progn
+           (set-face-attribute 'wg-brace-face nil
+                               :foreground "unspecified")
+           (set-face-attribute 'wg-command-face nil
+                               :foreground "unspecified")
+           (set-face-attribute 'wg-current-workgroup-face nil
+                               :foreground "unspecified")
+           (set-face-attribute 'wg-divider-face nil
+                               :foreground "unspecified")
+           (set-face-attribute 'wg-filename-face nil
+                               :foreground "unspecified")
+           (set-face-attribute 'wg-frame-face nil
+                               :foreground "unspecified")
+           (set-face-attribute 'wg-message-face nil
+                               :foreground "unspecified")
+           (set-face-attribute 'wg-mode-line-face nil
+                               :foreground "unspecified")
+           (set-face-attribute 'wg-other-workgroup-face nil
+                               :foreground "unspecified")
+           (set-face-attribute 'wg-previous-workgroup-face nil
+                               :foreground "unspecified")))))
