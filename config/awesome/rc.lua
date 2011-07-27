40c8c048db35ea4d4ae2f942e44d5a2c83e3d3eb
Chris Charles <chris@ccharles.ca>
2011-07-27 08:46:12 -0400

    Make Kopete windows float under Awesome.

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -321,6 +321,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "Gitk" },
       properties = { floating = true } },
+    { rule = { class = "Kopete" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
