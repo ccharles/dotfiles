244363f758d53fc8f31c91e89b2f93e313407080
Chris Charles <chris@ccharles.ca>
2011-07-30 11:15:41 -0400

    Make org-mode remove clock entries that show 0:00.

--- emacs.d/init.d/50org.el
+++ emacs.d/init.d/50org.el
@@ -16,6 +16,7 @@
   '(progn
      (global-set-key "\C-ca" 'org-agenda)
      (setq org-log-done 'time)
+     (setq org-clock-out-remove-zero-time-clocks t)
      (setq org-agenda-restore-windows-after-quit t)
      (setq org-hide-leading-stars t)
      (setq org-odd-levels-only t)))
