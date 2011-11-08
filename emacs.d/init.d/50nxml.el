e7571da6b395d26bfefaceb34c62bfb8296b44c7
Chris Charles <chris@ccharles.ca>
2011-11-08 12:00:07 -0500

    Added NXML config file.

---
+++ emacs.d/init.d/50nxml.el
@@ -0,0 +1,14 @@
+;; From Emacs prelude https://github.com/bbatsov/emacs-prelude
+(push '("<\\?xml" . nxml-mode) magic-mode-alist)
+
+;; CUA-rectangle mode clobbers the default X11 C-Return binding for
+;; nxml-complete, so let's use C-c C-c which doesn't appear to be used in
+;; nxml-mode.
+(add-hook 'nxml-mode-hook
+          (lambda ()
+            (define-key nxml-mode-map (kbd "C-c C-c") 'nxml-complete)))
+
+(eval-after-load 'nxml-mode
+  '(progn
+     (setq nxml-child-indent 4)
+     (setq nxml-attribute-indent 4)))
