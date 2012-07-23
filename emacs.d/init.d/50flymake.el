4d8725699972bf398a50f1ab85f5eaf335212da9
Chris Charles <chris@ccharles.ca>
2012-07-23 11:36:40 -0400

    Disable flymake for XML and HTML files.

--- emacs.d/init.d/50flymake.el
+++ emacs.d/init.d/50flymake.el
@@ -2,3 +2,6 @@
 
 (global-set-key (kbd "<f5>") 'flymake-goto-next-error)
 (global-set-key (kbd "<f6>") 'flymake-display-err-menu-for-current-line)
+
+(delete '("\\.html?\\'" flymake-xml-init) flymake-allowed-file-name-masks)
+(delete '("\\.xml\\'" flymake-xml-init) flymake-allowed-file-name-masks)
