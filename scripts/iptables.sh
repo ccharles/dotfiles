b30620d04ec751c21a5547f3aeb6ff86771ffbe2
Chris Charles <chris@ccharles.ca>
2012-07-23 11:37:45 -0400

    Add commented iptables rule to open a single port.

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -5,6 +5,7 @@ iptables --flush
 
 iptables --append INPUT --in-interface lo --jump ACCEPT
 iptables --append INPUT --match state --state ESTABLISHED,RELATED --jump ACCEPT
+# iptables -A INPUT -p tcp --dport 25 -j ACCEPT
 iptables --policy INPUT DROP
 
 iptables --policy FORWARD DROP
