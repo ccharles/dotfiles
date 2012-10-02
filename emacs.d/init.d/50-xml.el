7fa42ac9b5bc89625fe20d1a786d5249cc26820d
Chris Charles <chris@ccharles.ca>
2012-10-01 21:10:49 -0400

    Delete old nXML config in favour of new XML config.
    The contents are, actually, the same for now.

--- emacs.d/init.d/50-xml.el
+++ emacs.d/init.d/50-xml.el
@@ -1,9 +1,8 @@
 ;; From Emacs prelude https://github.com/bbatsov/emacs-prelude
 (push '("<\\?xml" . nxml-mode) magic-mode-alist)
 
-;; CUA-rectangle mode clobbers the default X11 C-Return binding for
-;; nxml-complete, so let's use C-c C-c which doesn't appear to be used in
-;; nxml-mode.
+;; I don't use CUA rectangle mode anymore, but now I'm used to this
+;; keybinding!
 (add-hook 'nxml-mode-hook
           (lambda ()
             (define-key nxml-mode-map (kbd "C-c C-c") 'nxml-complete)))
