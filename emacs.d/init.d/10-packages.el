f9c055dd18f71249fb9658fd82d30cd0aa8945d6
Chris Charles <chris@ccharles.ca>
2012-10-04 15:55:28 -0400

    Force package initalization so package configs work.

--- emacs.d/init.d/10-packages.el
+++ emacs.d/init.d/10-packages.el
@@ -3,14 +3,14 @@
 ;; Preference should be given to using package.el over either
 ;; operating-system packages or manual site-lisp stuff.
 
+(require 'package)
+(package-initialize)
+;; (add-to-list 'package-archives
+;;              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
 (defun install-base-packages ()
-  (require 'package)
-
-  ;; (add-to-list 'package-archives
-  ;;              '("marmalade" . "http://marmalade-repo.org/packages/") t)
-  (add-to-list 'package-archives
-               '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
   (package-refresh-contents)
 
   ;; TODO: What about updating the local database?
