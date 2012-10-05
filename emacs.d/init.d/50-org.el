0623f99ddfa8d4d04252a0d02a9bfb3a05b9b9fe
Chris Charles <chris@ccharles.ca>
2012-10-05 09:21:57 -0400

    Move org settings back while removing a few settings.

--- emacs.d/init.d/50-org.el
+++ emacs.d/init.d/50-org.el
@@ -1,17 +1,3 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;    Filename: 50org.el
-;;      Author: Chris Charles
-;;
-;; --------------------------------------------------------------------------
-;;
-;;    Org mode configuration file for Emacs.
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Enable newer Org-mode from AUR
-;; (require 'org-install nil t)
-
 (eval-after-load "org"
   '(progn
      (global-set-key "\C-ca" 'org-agenda)
@@ -20,8 +6,6 @@
      (setq org-agenda-restore-windows-after-quit t)
      (setq org-agenda-clockreport-parameter-plist
            '(:link t :maxlevel 5 :compact t))
-     (setq org-hide-leading-stars t)
-     (setq org-odd-levels-only t)
 
      (org-babel-do-load-languages 'org-babel-load-languages
                                   '((emacs-lisp . t)
