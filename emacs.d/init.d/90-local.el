a4c4545a0ce94ab4b7e3ac44f5c4b3fd56e3a252
Chris Charles <chris@ccharles.ca>
2012-10-05 06:27:52 -0400

    Move sample TRAMP proxy configuration into new local config.

--- emacs.d/init.d/90-local.el
+++ emacs.d/init.d/90-local.el
@@ -1,3 +1,14 @@
+;; Site-local Emacs settings.
+
+;; Set up TRAMP proxies
+;;
+;; (if (locate-library "tramp")
+;;     (eval-after-load "tramp"
+;;       '(add-to-list 'tramp-default-proxies-alist
+;;                     '("\\`my\\.server\\.tld\\'"
+;;                       "\\`my-user\\'"
+;;                       "/ssh:%h:"))))
+
 ;; Font for this system
 ;; (add-to-list 'default-frame-alist
 ;;              (cons 'font "Ubuntu Mono-10"))
