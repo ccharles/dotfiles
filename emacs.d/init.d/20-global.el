985cbc037f35f977208a1abbf470ab3da6eabce6
Chris Charles <chris@ccharles.ca>
2012-10-08 05:08:41 -0400

    Add whitespace settings.

--- emacs.d/init.d/20-global.el
+++ emacs.d/init.d/20-global.el
@@ -93,6 +93,43 @@
 ;; Show empty linesin the fringe
 (setq default-indicate-empty-lines t)
 
+;; Whitespace settings
+(if (locate-library "whitespace")
+    (autoload 'whitespace-mode "whitespace" nil t))
+
+(eval-after-load "whitespace"
+    '(progn
+       (setq whitespace-display-mappings
+             '((space-mark 32
+                           [183]
+                           [46])
+               (space-mark 160
+                           [164]
+                           [95])
+               (space-mark 2208
+                           [2212]
+                           [95])
+               (space-mark 2336
+                           [2340]
+                           [95])
+               (space-mark 3616
+                           [3620]
+                           [95])
+               (space-mark 3872
+                           [3876]
+                           [95])
+               (newline-mark 10
+                             [182 10]
+                             [36 10])
+               (tab-mark 9
+                         [187 9]
+                         [92 9])))
+
+       (setq whitespace-style
+             '(face tabs spaces trailing space-before-tab newline indentation
+                    empty space-after-tab space-mark tab-mark newline-mark))))
+
+
 ;; A bunch of plugins that I always want.
 
 (if (locate-library "ace-jump-mode")
