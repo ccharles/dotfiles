a9b06d3e13c3aadc6b9e06579c784c321f452452
Chris Charles <chris@ccharles.ca>
2013-03-02 13:55:24 -0500

    Temporarily disable php+-mode

--- emacs.d/init.d/50-php.el
+++ emacs.d/init.d/50-php.el
@@ -1,8 +1,8 @@
 ;; PHP config
 
-(add-to-list 'auto-mode-alist
-             '("\\.php[45]?\\'" . php+-mode))
+;; (add-to-list 'auto-mode-alist
+;;              '("\\.php[45]?\\'" . php+-mode))
 
-(autoload 'php+-mode "php+-mode")
-(eval-after-load "php+-mode"
-  '(php+-mode-setup))
+;; (autoload 'php+-mode "php+-mode")
+;; (eval-after-load "php+-mode"
+;;   '(php+-mode-setup))
